b9fef0317abb43dfa7ef46a2af1b8c282759d4d0
Florian Gauger <flo.gauger@gmail.com>
2016-07-30 21:44:54 +0200

    Add dependencies and working directory

--- src/main/java/edu/kit/robozen/example/BasicMainWin.java
+++ src/main/java/edu/kit/robozen/example/BasicMainWin.java
@@ -17,14 +17,21 @@ public class BasicMainWin {
 
     private static Logger logger = Logger.getLogger(BasicMainWin.class.getName());
 
+    public final static String workingDirectory = "example_working_dir";
+    public final static String log4jConfig = "src/main/resources/log4j.properties";
+    public final static String serverDir = "example_working_dir";
+    public final static String serverExe = "example_working_dir";
+    public final static String monitorDir = "example_working_dir";
+    public final static String monitorExe = "example_working_dir";
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Util.InitWinConfiguration initConfiguration = new Util.InitWinConfiguration(workingDirectory, log4jConfig, serverDir, serverExe, monitorDir, monitorExe);
 
         PerPlayModePolicy perPlayModePolicy = new PerPlayModePolicy(new ChaseAndKickPolicy());
         perPlayModePolicy.replacePolicyForPlayMode(new KickOffPolicy(), PlayMode.KICK_OFF_EAST, PlayMode.KICK_OFF_WEST, PlayMode.GOAL_SIDE_EAST, PlayMode.GOAL_SIDE_WEST);
         perPlayModePolicy.replacePolicyForPlayMode(new BeforeGamePolicy(), PlayMode.UNKNOWN);
 
         Util.TeamDescription teamDescription = new Util.TeamDescription(perPlayModePolicy, 5, new ArrayList<>());
-        Util.executeGameWin("example_working_dir", "src/main/resources", teamDescription, teamDescription, t -> t.moveBall(new Ball(0, 0)));
+        Util.executeGameWin(initConfiguration, teamDescription, teamDescription, t -> t.moveBall(new Ball(0, 0)));
     }
 }
